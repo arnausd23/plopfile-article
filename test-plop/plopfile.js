f0d61978645e0cad46e38e552d28f76384560954
arnau <arnausd23@hotmail.com>
2019-07-10 18:57:11 +0200

    Added files to generate action file

--- test-plop/plopfile.js
+++ test-plop/plopfile.js
@@ -1,29 +1,43 @@
 module.exports = function (plop) {
-    plop.setGenerator('components', {
-        description: 'This will generate both the components and the containers folder',
-        prompts: [{
-            type: 'input',
-            name: 'name',
-            message: 'type the name for your components folder'
-        }],
-        actions: [{
-            type: 'add',
-            path: 'src/components/{{name}}.js',
-            templateFile: 'plop-templates/components.hbs'
-        }]
-    });
-
-    plop.setGenerator('container', {
-        description: 'This will generate both the components and the containers folder',
-        prompts: [{
-            type: 'input',
-            name: 'name',
-            message: 'type the name for containers folder'
-        }],
-        actions: [{
-            type: 'add',
-            path: 'src/containers/{{name}}.js',
-            templateFile: 'plop-templates/container.hbs'
-        }]
+    plop.setGenerator('actions', {
+        description: 'This will generate the actions file',
+        prompts: [
+            {
+                type: 'input',
+                name: 'fileName',
+                message: 'type the name for your file'
+            },
+            {
+                type: 'input',
+                name: 'actionName',
+                message: 'type the name for your action'
+            }
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: '{{pascalCase fileName}}.actions.ts',
+                templateFile: 'plop-templates/actions/main.hbs',
+                skipIfExists: true
+            },
+            {
+                type: 'append',
+                path: '{{pascalCase fileName}}.actions.ts',
+                pattern: /(\/\/ constants)/g,
+                templateFile: 'plop-templates/actions/constants.hbs',
+            },
+            {
+                type: 'append',
+                path: '{{pascalCase fileName}}.actions.ts',
+                pattern: /(\/\/ actions)/g,
+                templateFile: 'plop-templates/actions/actions.hbs',
+            },
+            {
+                type: 'append',
+                path: '{{pascalCase fileName}}.actions.ts',
+                pattern: /(export\s+type\s[\w]+\s=)/g,
+                templateFile: 'plop-templates/actions/typeExport.hbs',
+            }
+        ]
     });
 };
